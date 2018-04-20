0a9645367ac2e0db73f8d3caf8a0f865c8aeb75f
Masen Matthews <masenmatthews@gmail.com>
2018-04-20 14:25:31 -0700

    fix database and seeding issue - now displaying 50 products and 250 reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
 Product.destroy_all
 Review.destroy_all
 
-30.times do |i|
+50.times do |i|
   Product.create!(title: Faker::DragonBall.character, description: Faker::Overwatch.quote)
   @product_id = Product.last.id
-  10.times do |i|
-    @description = Faker::RickAndMorty.quote
-    title = Faker::Fallout.quote
+  5.times do |i|
+    @review_description = Faker::RickAndMorty.quote
+    review_title = Faker::Fallout.quote
     @rating = Faker::Number.between(1, 5)
-    Review.create!(description: @description, rating: @rating, title: title, :product_id => @product_id)
+    Review.create!(review_description: @review_description, rating: @rating, review_title: review_title, :product_id => @product_id)
   end
 end
 
