cc3e168309da668f397ff6d36988d54c3b6e7bfe
Masen Matthews <masenmatthews@gmail.com>
2018-04-20 11:24:04 -0700

    fix typo, 7/7 specs passing

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,5 +3,5 @@ require 'rails_helper'
 describe Product do
   it { should validate_presence_of :title }
   it { should validate_presence_of :description }
-  it { should have_many :review }
+  it { should have_many :reviews }
 end
