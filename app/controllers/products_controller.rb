e95e1ec0a471f8de1ed4f0da2235261120945458
Masen Matthews <masenmatthews@gmail.com>
2018-04-25 10:03:40 -0700

    fix delete method for removing products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,32 +1,14 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.all
-    render :index
-  end
-
-  def show
-    @product = Product.find(params[:id])
-    render :show
   end
 
   def new
     @product = Product.new
-    render :new
-  end
-
-  def create
-    @product = Product.new(product_params)
-    if @product.save
-      flash[:notice] = "Product successfully added!"
-      redirect_to products_path
-    else
-      render :new
-    end
   end
 
   def edit
     @product = Product.find(params[:id])
-    render :edit
   end
 
   def update
@@ -41,8 +23,22 @@ class ProductsController < ApplicationController
   def destroy
     @product = Product.find(params[:id])
     @product.destroy
-    flash[:alert] = "Product successfully deleted!"
+    flash[:notice] = "Product successfully deleted!"
     redirect_to products_path
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      flash[:notice] = "Product successfully added!"
+      redirect_to products_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @product = Product.find(params[:id])
   end
 
   private
