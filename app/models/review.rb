2a625540925e6af5561e7422e1ce00a397dcd3bf
Masen Matthews <masenmatthews@gmail.com>
2018-04-26 14:57:38 -0700

    add validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,8 @@
 class Review < ActiveRecord::Base
   belongs_to :product
   validates :review_title, :review_description, :rating, :presence => true
+  validates :review_description, length: { minimum: 50 }
+  validates :review_description, length: { maximum: 250 }
+  validates :rating, numericality: { greater_than_or_equal_to: 1 }
+  validates :rating, numericality: { less_than_or_equal_to: 5 }
 end
