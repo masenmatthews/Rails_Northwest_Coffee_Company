0fc39fe60108a6028461fab758d1c03d5d4c8d69
Masen Matthews <masenmatthews@gmail.com>
2018-04-26 10:55:30 -0700

    add country-of-origin fields and tests

--- spec/features/add_review_pages_spec.rb
+++ spec/features/add_review_pages_spec.rb
@@ -2,18 +2,19 @@ require 'rails_helper'
 
 describe "the add a review process" do
   it "add a new review" do
-  visit new_product_path
-  fill_in 'Title', :with => 'Grapes'
-  fill_in 'Description', :with => 'Small round fruits'
-  click_on 'Create Product'
-  expect(page).to have_content 'Grapes'
-  click_on 'Grapes'
-  click_on 'Add new review'
-  fill_in 'Review title', :with => 'Satisfactory purchase'
-  fill_in 'Review description', :with => 'Grapes taste good'
-  fill_in 'Rating', with: '4'
-  click_on 'Create Review'
-  expect(page). to have_content 'Satisfactory purchase'
-  end
+    visit new_product_path
+    fill_in 'Title', :with => 'Grapes'
+    fill_in 'Description', :with => 'Small round fruits'
+    fill_in 'Country of Origin', :with => 'USA'
 
+    click_on 'Create Product'
+    expect(page).to have_content 'Grapes'
+    click_on 'Grapes'
+    click_on 'Add new review'
+    fill_in 'Review title', :with => 'Satisfactory purchase'
+    fill_in 'Review description', :with => 'Grapes taste good'
+    fill_in 'Rating', with: '4'
+    click_on 'Create Review'
+    expect(page). to have_content 'Satisfactory purchase'
+  end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 describe Product do
   it { should validate_presence_of :title }
   it { should validate_presence_of :description }
+  it { should validate_presence_of :country }
   it { should have_many :reviews }
 end
