17a2e96ed0c11113d5a598fb04a52268d46e4ca4
Masen Matthews <masenmatthews@gmail.com>
2018-04-26 15:17:43 -0700

    work on scopes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,9 @@ class HomeController < ApplicationController
   def index
     @products = Product.all
     @reviews = Review.all
-
+    @made_in_usa = Product.made_in_usa
+    @three_most_recent = Product.three_most_recent
+    @most_reviewed_products = Product.most_reviewed_products
     render :index
   end
 end
