6c075edbfe37566942729469b99083337ac46e72
Masen Matthews <masenmatthews@gmail.com>
2018-07-19 14:05:34 -0700

    rework seed file to be coffee-related, remove jumbotron (goodbye forever), edit styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,11 +2,11 @@ Product.destroy_all
 Review.destroy_all
 
 50.times do |i|
-  Product.create!(title: Faker::Food.dish, description: Faker::Coffee.notes, country: Faker::Address.country, created_at: Faker::Date.between(365.days.ago, Date.today) )
+  Product.create!(title: Faker::Coffee.blend_name, description: Faker::Coffee.variety, country: Faker::Coffee.origin, created_at: Faker::Date.between(365.days.ago, Date.today) )
   @product_id = Product.last.id
   5.times do |i|
     @review_description = Faker::Lorem.characters(200)
-    review_title = Faker::Overwatch.quote
+    review_title = Faker::Coffee.notes
     @rating = Faker::Number.between(1, 5)
     Review.create!(review_description: @review_description, rating: @rating, review_title: review_title, :product_id => @product_id)
   end
